5613bffe3eb201e4d7c0352f250149ea4b09482b
dewofthesea <rosemaryita555@gmail.com>
2022-03-11 09:47:00 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		char hello[] = "\"Programming is like building a multilingual puzzle";
+	char hello[] = "\"Programming is like building a multilingual puzzle";
 
-			puts(hello);
-				return (0);
+	puts(hello);
+	return (0);
 }
